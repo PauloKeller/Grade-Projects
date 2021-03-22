4cc19a249f0e4a7097ac0a4d342b58dd9ae3b0ee
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-03-21 21:08:44 -0300

    refactore the calculator class to remove the constructor dependencie

--- lab_bd_vi/pratica_01/src/main/java/Calculator.java
+++ lab_bd_vi/pratica_01/src/main/java/Calculator.java
@@ -1,9 +1,10 @@
+
 public class Calculator {
-  public double customLog(double base, double logNumber) {
+  static public double customLog(double base, double logNumber) {
     return Math.log(logNumber) / Math.log(base);
   }
 
-  public Complex complexLog(Complex base, Complex logNumber) {
+  static public Complex complexLog(Complex base, Complex logNumber) {
     final double real = Math.log(logNumber.real()) / Math.log(base.real());
     final double imaginary = Math.log(logNumber.imag()) / Math.log(base.imag());
 
--- lab_bd_vi/pratica_01/src/test/java/CalculatorTests.java
+++ lab_bd_vi/pratica_01/src/test/java/CalculatorTests.java
@@ -2,25 +2,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorTest {
+public class CalculatorTests {
 
   @Test
   public void testCustomLogarithm() {
-    Calculator calculator = new Calculator();
     double base = 2.0;
     double logNumber = 4.0;
     double expected = 2;
 
-    assertEquals(calculator.customLog(base, logNumber), expected);
+    assertEquals(Calculator.customLog(base, logNumber), expected);
   }
 
   @Test
   public void testComplexLogarithm() {
-    Calculator calculator = new Calculator();
     Complex base = new Complex(2.0, 2.0);
     Complex logNumber = new Complex(4.0, 1);
     Complex expected = new Complex(2.0, 0.0);
-    Complex stub = calculator.complexLog(base, logNumber);
+    Complex stub = Calculator.complexLog(base, logNumber);
 
     assertEquals(stub.real(), expected.real());
     assertEquals(stub.imag(), expected.imag());
